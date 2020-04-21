161ba0797ef078285169ed451a458c16beb2613d
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-04-21 22:37:43 +0100

    added internal jumbotron

--- src/components/DataBox.vue
+++ src/components/DataBox.vue
@@ -1,27 +1,39 @@
 <template>
   <div class="data-box-container">
     <b-jumbotron>
-      <template v-slot:lead>
-          
-          {{ inputdata.filename }}
-
-      </template>
+      <template v-slot:lead>{{ inputdata.filename }}</template>
 
       <hr class="my-4" />
+      <b-jumbotron class="internal">
+        <p v-for="[m_info, index] in inputdata.contentType" :key="index">{{ m_info }}</p>
+      </b-jumbotron >
 
-      <p>Hadith</p>
-
-      <b-button variant="primary" href="#">Next button</b-button>
       <b-button @click="next" variant="success" href="#">Next</b-button>
+      <b-button variant="primary" href="#">Random</b-button>
     </b-jumbotron>
   </div>
 </template>
 
 <script>
 export default {
-    props: {
-        inputdata: Object,
-        next: Function
+  props: {
+    inputdata: Object,
+    next: Function
+  },
+  computed: {
+    more_info() {
+      console.log("stuff could happen here");
+      return "info";
     }
-}
+  }
+};
 </script>
+
+<style scoped>
+.internal {
+    margin-bottom: 10px;
+}
+.btn {
+  margin: 0 5px;
+}
+</style>
