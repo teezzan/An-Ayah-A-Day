d65527207698ed0a043f8bf96a215de4ae570c2c
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-05-05 10:36:35 +0100

    hid player

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,30 +1,30 @@
 <template>
-  <b-row align-h="center"  >
-
-      <b-col cols="6" align-self="end" class="player">
-    <audio controls ref="audplay">
-      <source :src="aud" type="audio/mpeg" />Your browser does not support the audio element.
-    </audio><br /><b-button variant="primary" class="p10" @click="pause">Pause/Play</b-button>
+  <b-row align-h="center">
+    <b-col cols="6" align-self="end" class="player">
+      <b-button variant="primary" class="p10" @click="pause">Pause/Play</b-button>
+      <audio controls ref="audplay" style="display: none">
+        <source :src="aud" type="audio/mpeg" />Your browser does not support the audio element.
+      </audio>
+      <br />
+      
     </b-col>
   </b-row>
 </template>
 
 <script>
 export default {
-  data (){
-    return{
-      aud: this.audioUrl,
-    }
+  data() {
+    return {
+      aud: this.audioUrl
+    };
   },
   props: {
-    audioUrl: String,
+    audioUrl: String
   },
   methods: {
     pause() {
-      if (this.$refs.audplay.paused)
-      this.$refs.audplay.play();
-      else
-          this.$refs.audplay.pause();
+      if (this.$refs.audplay.paused) this.$refs.audplay.play();
+      else this.$refs.audplay.pause();
     },
     update() {
       (this.$refs.audplay.src = this.audioUrl), this.$refs.audplay.play();
@@ -34,25 +34,22 @@ export default {
   watch: {
     audioUrl: function() {
       // this.$refs.audplay.src = this.audioUrl;
-      if (this.$refs.audplay.paused){
+      if (this.$refs.audplay.paused) {
         this.$refs.audplay.src = this.audioUrl;
         this.$refs.audplay.play();
         this.$refs.audplay.pause();
-      }
-      else{
+      } else {
         this.$refs.audplay.src = this.audioUrl;
         this.$refs.audplay.pause();
         this.$refs.audplay.play();
       }
-      
     }
   }
 };
 </script>
 
 <style scoped>
-.player{
+.player {
   margin-bottom: 180px;
-
 }
 </style>
