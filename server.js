1e39e7c20cc30825ff41905094c56d210f6b52a4
teezzan <teehazzan@gmail.com>
2020-06-04 12:53:11 +0100

    solving server problem

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
-app = express();
+var history = require('connect-history-api-fallback');
+var app = express();
+app.use(history());
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
+
+app.get('*', function(req, res) {
+    res.redirect('/');
+});
+
+
+
+const port = process.env.PORT || 8080;
 
 app.listen(port);
