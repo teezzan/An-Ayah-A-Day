69730d1e0e156a609ac36c43b481a0e4f40b3f5a
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-07-03 22:31:49 +0100

    began jimp implementation

--- server.js
+++ server.js
@@ -2,16 +2,58 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 var history = require('connect-history-api-fallback');
+var Jimp = require('jimp');
+var fs = require('fs');
 var app = express();
 app.use(history());
 app.use(serveStatic(path.join(__dirname, 'dist')));
 
-app.get('*', function(req, res) {
-    res.redirect('/');
+// app.get('*', function (req, res) {
+//     res.redirect('/');
+// });
+
+app.get('*', (req, res) => {
+
+
+
+    Jimp.read('./public/0001.jpg', (err, lenna) => {
+        if (err) throw err;
+        lenna
+            // .resize(256, 256) // resize
+            .quality(100) // set JPEG quality
+            .greyscale() // set greyscale
+            .crop( 20, 20, 300, 250 ) 
+            .write('lena-small-bw.jpg', (err) => {
+                console.log(err);
+                // res.sendFile("./lena-small-bw.jpg", (err) => {
+                //     if (err) return res.status(404);
+                //     console.log("GOOD");
+                // });
+                res.sendFile(path.join(__dirname, '/lena-small-bw.jpg'));
+
+            });
+
+    });
+    // var tee = fs.createWriteStream("./lena-small-bw.jpg");
+    // console.log(tee);
+
+
+
+})
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+    console.log("Listening onport 8080");
+
 });
 
 
 
-const port = process.env.PORT || 8080;
 
-app.listen(port);
+
+
+
+// res.status(200).sendFile("/lena-small-bw.jpg", (err)=>{
+//     if(err) console.log("done");
+// });
