52e474d0431b1f4c7ca922854d8de90364d08405
Taiwo Hassan Yusuf <teehazzan@gmail.com>
2020-07-04 21:10:33 +0100

    added image generator

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ var history = require('connect-history-api-fallback');
 var Jimp = require('jimp');
 var fs = require('fs');
 var app = express();
+var bodyParser = require('body-parser');
 app.use(history());
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('*', function (req, res) {
     res.redirect('/');
@@ -16,18 +19,18 @@ app.post('*', (req, res) => {
 
 
 
-    Jimp.read('./public/0001.jpg', (err, lenna) => {
+    Jimp.read(`http://genpix.herokuapp.com/api/render?url=https://quran.com/${req.body.surah}/${req.body.ayah}&output=screenshot&viewport.height=2000&viewport.width=1200`, (err, lenna) => {
         if (err) throw err;
         lenna
             // .resize(256, 256) // resize
             .quality(100) // set JPEG quality
             .greyscale() // set greyscale
-            .crop( 20, 20, 300, 250 ) 
+            // .crop( 20, 20, 300, 250 ) 
             .write('lena-small-bw.jpg', (err) => {
                 console.log(err);
                 // res.sendFile("./lena-small-bw.jpg", (err) => {
                 //     if (err) return res.status(404);
-                    console.log(path.join(__dirname, '/lena-small-bw.jpg'));
+                console.log(path.join(__dirname, '/lena-small-bw.jpg'));
                 // });
                 res.sendFile(path.join(__dirname, '/lena-small-bw.jpg'));
 
